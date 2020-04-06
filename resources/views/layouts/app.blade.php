c7dcb07d1d9255eaf48557e324aaf7decbe4b1c8
Pati <psritanyarath@infina.net>
2020-04-05 22:38:13 -0400

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	
+	<title>{{ config('app.name', 'Laravel') }}</title>
+	
+	<!-- Styles -->
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<!-- Bootstrap 4.4.1 CDN CSS -->
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+	      rel="stylesheet"
+	      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+	      crossorigin="anonymous">
+
+
+</head>
+<body>
+<div id="app">
+	<nav class="navbar navbar-default navbar-static-top">
+		<div class="container">
+			<div class="navbar-header">
+				
+				<!-- Collapsed Hamburger -->
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+					<span class="sr-only">Toggle Navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				
+				<!-- Branding Image -->
+				<a class="navbar-brand" href="{{ url('/') }}">
+					{{ config('app.name', 'Laravel') }}
+				</a>
+			</div>
+			
+			<div class="collapse navbar-collapse" id="app-navbar-collapse">
+				<!-- Left Side Of Navbar -->
+				<ul class="nav navbar-nav">
+					&nbsp;
+				</ul>
+				
+				<!-- Right Side Of Navbar -->
+				<ul class="nav navbar-nav navbar-right">
+					<!-- Authentication Links -->
+					@if (Auth::guest())
+						<li><a href="{{ route('login') }}">Login</a></li>
+						<li><a href="{{ route('register') }}">Register</a></li>
+					@else
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+								{{ Auth::user()->name }} <span class="caret"></span>
+							</a>
+							
+							<ul class="dropdown-menu" role="menu">
+								<li>
+									<a href="{{ route('logout') }}"
+									   onclick="event.preventDefault();
+									   document.getElementById('logout-form').submit();">
+										Logout
+									</a>
+									
+									<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+										{{ csrf_field() }}
+									</form>
+								</li>
+							</ul>
+						</li>
+					@endif
+				</ul>
+			</div>
+		</div>
+	</nav>
+	
+	@yield('content')
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+<!-- Bootstrap 4.4.1 CDN JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous">
+	
+</script>
+</body>
+</html>
